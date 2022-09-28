ebcb6654c32cfb05ccc62813dde61208de9d8446
edu1010 <39130123+edu1010@users.noreply.github.com>
2022-09-28 02:32:31 +0200

    First node of the graph generated automatically

--- Assets/DialogueSystem/Editor/DialogueGraph.cs
+++ Assets/DialogueSystem/Editor/DialogueGraph.cs
@@ -3,7 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Experimental.GraphView;
 using UnityEditor;
+using UnityEngine.UIElements;
 public class DialogueGraph : EditorWindow
 {
-   
+    private DialogueGraphView graphView;
+    [MenuItem("Graph/Dialogue graph by Edu ;)")]
+   public static void OpenDialogueGraphWindow()
+    {
+        var window = GetWindow<DialogueGraph>();
+        window.titleContent = new GUIContent("Dialogue graph by Edu");
+    }
+    private void OnEnable()
+    {
+        graphView = new DialogueGraphView 
+        {
+            name = "Dialogue graph"
+        };
+        graphView.StretchToParentSize();
+        rootVisualElement.Add(graphView);
+    }
+    private void OnDisable()
+    {
+        rootVisualElement.Remove(graphView);
+    }
 }
--- Assets/DialogueSystem/Editor/DialogueGraphView.cs
+++ Assets/DialogueSystem/Editor/DialogueGraphView.cs
@@ -2,7 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.Experimental.GraphView;
+using UnityEngine.UIElements;
+using System;
+
 public class DialogueGraphView : GraphView
 {
-    
+    public DialogueGraphView()
+    {
+        this.AddManipulator(new ContentDragger());
+        this.AddManipulator(new SelectionDragger());
+        this.AddManipulator(new RectangleSelector());
+        AddElement(GenerateEntryPointNode());
+    }
+
+    private DialogueNode GenerateEntryPointNode()
+    {
+        var node = new DialogueNode
+        {
+            title = "Start",
+            GUID = Guid.NewGuid().ToString(),
+            DialogueText = "ENTRYPOINT",
+            EntryPoint = true
+        };
+        node.SetPosition(new Rect(100, 200, 100, 150));
+        return node;
+    }
 }
