35236e1e1a68f61ca841771a259c4f15530b5572
edu1010 <39130123+edu1010@users.noreply.github.com>
2023-01-06 00:58:49 +0100

    combate cosas

--- Assets/puzzleCombat.cs
+++ Assets/puzzleCombat.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Fungus;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 
@@ -13,7 +14,9 @@ public class puzzleCombat : MonoBehaviour
     private Character _enemy;
     public TextMeshProUGUI enemyName;
     private float hplayer = 100;
-    private float henemy = 100;
+    private float hpenemy = 100;
+    public Slider sliderPlayer;
+    public Slider sliderEnemy;
     public Flowchart flowchartCombat;
     private StringVariable conversation;
 
@@ -36,7 +39,7 @@ public class puzzleCombat : MonoBehaviour
         _enemy = enemy;
         _nextNode = nextNode;
         hplayer = 100;
-        henemy = 100;
+        hpenemy = 100;
         enemyName.text = enemy.NameText;
     }
     
@@ -68,9 +71,70 @@ public class puzzleCombat : MonoBehaviour
 
     }
 
-    public void PlayerAnswer(AttackType player)
+    public void PlayerAnswer(AttackType playerAttack)
     {
-        
+        switch (playerAttack)
+        {
+            case AttackType.Guardia:
+                if (enemyAttack == AttackType.Guardia)
+                {
+                    //nada
+                }
+
+                if (enemyAttack == AttackType.Golpe)
+                {
+                    hpenemy -= 20;
+                }
+
+                if (enemyAttack == AttackType.Patada)
+                {
+                    hplayer -= 20;
+                }
+                break;
+            case AttackType.Golpe:
+
+                if (enemyAttack == AttackType.Guardia)
+                {
+                    hplayer -= 20;
+                }
+
+                if (enemyAttack == AttackType.Golpe)
+                {
+                    hplayer -= 20;
+                    hpenemy -= 20;
+                }
+
+                if (enemyAttack == AttackType.Patada)
+                {
+                    hpenemy -= 20;
+                }
+                break;
+            case AttackType.Patada:
+
+                if (enemyAttack == AttackType.Guardia)
+                {
+                    hpenemy -= 20;
+                }
+
+                if (enemyAttack == AttackType.Golpe)
+                {
+                    hplayer -= -20;
+                }
+
+                if (enemyAttack == AttackType.Patada)
+                {
+                    //nada
+                }
+                break;
+        }
+
+        SetHealhBar();
+    }
+
+    public void SetHealhBar()
+    {
+        sliderPlayer.value = hplayer;
+        sliderEnemy.value = hpenemy;
     }
     public enum AttackType
     {
