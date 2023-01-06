9e3f4c4af70e32f83b1c72b8f95b776e21bd3b62
edu1010 <39130123+edu1010@users.noreply.github.com>
2023-01-06 06:20:28 +0100

    desarrollo

--- Assets/puzzleCombat.cs
+++ Assets/puzzleCombat.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Fungus;
 using TMPro;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
@@ -17,23 +18,34 @@ public class puzzleCombat : MonoBehaviour
     private float hpenemy = 100;
     public Slider sliderPlayer;
     public Slider sliderEnemy;
+    public Slider sliderTimer;
+    public float MaxTime = 1;
+    private float _elapsedTime=0;
     public Flowchart flowchartCombat;
     private StringVariable conversation;
 
     public List<String> frasesGuardia = new List<string>();
     public List<String> frasesGolpe = new List<string>();
-    public List<String> frasesPatada = new List<string>();
-    public TextMeshProUGUI butonGuardia;
-    public TextMeshProUGUI butonGolpe;
-    public TextMeshProUGUI butonPatada;
+    public List<String> frasesPatada = new List<string>(); 
+    
+    public List<String> frasesGuardiaEnemy = new List<string>();
+    public List<String> frasesGolpeEnemy   = new List<string>();
+    public List<String> frasesPatadaEnemy  = new List<string>();
+    public Text butonGuardia;
+    public Text butonGolpe;
+    public Text butonPatada;
     public AttackType enemyAttack;
+    
+    //para pruebas
+    public Character ch1;
     private void Awake()
     {
-        
-        SetConversation();
+
+        ActivatePuzzle(ch1, "");
     }
 
-    [SerializeField]
+    private int _decisionEnemy = 0;
+    private string _fraseEnemigo;
     public void ActivatePuzzle(Character enemy,string nextNode)
     {
         _enemy = enemy;
@@ -41,8 +53,32 @@ public class puzzleCombat : MonoBehaviour
         hplayer = 100;
         hpenemy = 100;
         enemyName.text = enemy.NameText;
+        sliderPlayer.maxValue = 100;
+        sliderEnemy.maxValue = 100;
+        sliderTimer.maxValue = MaxTime;
+        SetHealhBar();
+        BucleConversationStart();
     }
-    
+
+    public void BucleConversationStart()
+    {
+        _decisionEnemy = Random.Range(0, 3);
+        switch (_decisionEnemy)
+        {
+            case 0:
+                _fraseEnemigo = frasesGuardiaEnemy[Random.Range(0,frasesGuardiaEnemy.Count)];
+                SetConversation(AttackType.Guardia, _fraseEnemigo);
+                break;
+            case 1:
+                _fraseEnemigo = frasesGolpeEnemy[Random.Range(0,frasesGolpeEnemy.Count)];
+                SetConversation(AttackType.Golpe, _fraseEnemigo);
+                break;
+            case 2:
+                _fraseEnemigo = frasesPatadaEnemy[Random.Range(0,frasesPatadaEnemy.Count)];
+                SetConversation(AttackType.Patada, _fraseEnemigo);
+                break;
+        }
+    }
 
     public void DesactivatePuzle()
     {
@@ -51,28 +87,63 @@ public class puzzleCombat : MonoBehaviour
     }
 
 
-    public void SetConversation()
+    public void SetConversation(AttackType attack, string frase)
     {
-        flowchartCombat.GetVariable<StringVariable>("Conversation").Value = "Reinhard Mittermeyer reinhard_test: Holi";
+        string txt = _enemy.NameText + ":" + frase;
+        
+        //flowchartCombat.GetVariable<StringVariable>("Conversation").Value = $"Reinhard Mittermeyer reinhard_test: Holi";
+        flowchartCombat.GetVariable<StringVariable>("Conversation").Value = txt;
+        flowchartCombat.ExecuteBlock("EnemyText");
+        SetAnswers();
     }
 
     public void SetAnswers()
     {
-        butonGuardia.text = frasesGuardia[Random.Range(0, frasesGuardia.Count)];
-        butonGolpe.text = frasesGolpe[Random.Range(0, frasesGuardia.Count)];
-        butonPatada.text = frasesPatada[Random.Range(0, frasesGuardia.Count)];
-    }
-
-    public void prueba()
-    {
-        flowchartCombat.GetVariable<StringVariable>("Conversation").Value=" Reinhard Mittermeyer reinhard_test  Hide Reinhard: chao";
-        flowchartCombat.GetVariable<StringVariable>("Conversation").Value=" Erberk Demir: chao";
-        flowchartCombat.ExecuteBlock("EnemyText");
+        butonGuardia.text = frasesGuardia[Random.Range(0, frasesGuardia.Count-1)];
+        butonGolpe.text = frasesGolpe[Random.Range(0, frasesGuardia.Count-1)];
+        butonPatada.text = frasesPatada[Random.Range(0, frasesGuardia.Count-1)];
+        _elapsedTime = 0f;
+       
+        sliderTimer.value = MaxTime;
+        restTime = true;
 
     }
 
-    public void PlayerAnswer(AttackType playerAttack)
+    private bool restTime = false;
+    public void Update()
     {
+        if (restTime)
+        {
+            _elapsedTime += Time.deltaTime;
+            sliderTimer.value -= Time.deltaTime;
+            if (_elapsedTime > MaxTime)
+            {
+                _elapsedTime = 0;
+                restTime = false;
+            }
+        }
+    }
+
+  
+    public void PlayerAnswer(int decision)//Button call thiis function
+    {
+        switch (decision)
+        {
+            case 0:
+                PlayerAnswer(AttackType.Guardia);
+                break;
+            case 1:
+                PlayerAnswer(AttackType.Golpe);
+                break;
+            case 2:
+                PlayerAnswer(AttackType.Patada);
+                break;
+        }
+    }
+    private void PlayerAnswer(AttackType playerAttack)
+    {
+        restTime = false;
+        _elapsedTime = 0;
         switch (playerAttack)
         {
             case AttackType.Guardia:
@@ -129,17 +200,50 @@ public class puzzleCombat : MonoBehaviour
         }
 
         SetHealhBar();
+        if (hpenemy > 0 && hplayer > 0)
+        {
+            BucleConversationStart();
+        }
+        else
+        {
+            if (hplayer > 0)
+            {
+                //win
+                PlayerWin();
+            }
+            else
+            {
+                ResetCombat();
+            }
+        }
     }
 
+    public void PlayerWin()
+    {
+        FlowChartManager.Instance.CallBlock(_nextNode);
+        DesactivatePuzle();
+    }
+
+    public void ResetCombat()
+    {
+        ActivatePuzzle(_enemy,_nextNode);
+    }
     public void SetHealhBar()
     {
         sliderPlayer.value = hplayer;
         sliderEnemy.value = hpenemy;
     }
-    public enum AttackType
+
+    public void prueba()
     {
-        Guardia,
-        Golpe,
-        Patada
-    }
+        flowchartCombat.GetVariable<StringVariable>("Conversation").Value=" Reinhard Mittermeyer reinhard_test  Hide Reinhard: chao";
+        flowchartCombat.GetVariable<StringVariable>("Conversation").Value=" Erberk Demir: chao";
+        flowchartCombat.ExecuteBlock("EnemyText");
+    }   
+}
+public enum AttackType
+{
+    Guardia,
+    Golpe,
+    Patada
 }
