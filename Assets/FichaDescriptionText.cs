941db67b0c625ca29d38dda8c9ebec8042ace2df
edu <eduard.corral@accenture.com>
2023-01-09 08:51:58 +0100

    fix ficha

--- Assets/FichaDescriptionText.cs
+++ Assets/FichaDescriptionText.cs
@@ -8,12 +8,14 @@ public class FichaDescriptionText : MonoBehaviour
     public static FichaDescriptionText Instance;
 
     public TextMeshProUGUI textDesc;
+    public GameObject goToDesactivate;
     // Start is called before the first frame update
     void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
+            goToDesactivate.SetActive(false);
         }
         else
         {
